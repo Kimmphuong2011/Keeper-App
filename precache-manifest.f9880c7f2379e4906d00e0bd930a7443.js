7e01085d758b22433f5ff44640a6bba97ca36de6
KP <kp@KP-2.local>
2024-07-02 09:57:09 +0700

    Updates

---
+++ precache-manifest.f9880c7f2379e4906d00e0bd930a7443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96262c8e3ae2891da6fd4e4543c477f",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "3c75567718a86406afaa",
+    "url": "/Keeper-App/static/js/2.5b018ae3.chunk.js"
+  },
+  {
+    "revision": "a3f9a922bad92e7e4407",
+    "url": "/Keeper-App/static/js/3.76d54c8d.chunk.js"
+  },
+  {
+    "revision": "c4303c16f97e3efc8136",
+    "url": "/Keeper-App/static/js/main.4ce75b0d.chunk.js"
+  },
+  {
+    "revision": "b15b179a0b44d63ea4ae",
+    "url": "/Keeper-App/static/js/runtime~main.df06fdf1.js"
+  }
+]);
