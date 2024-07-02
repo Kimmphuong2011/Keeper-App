582fef579bbcecaa61bbe03832010c08c48e52a1
Phuong Thai <151762246+Kimmphuong2011@users.noreply.github.com>
2024-07-02 09:42:52 +0700

    Add files via upload

---
+++ build/precache-manifest.ee144ad615ca2e7c9dee274e734a14b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadba996d032999d560353171ddd18a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c75567718a86406afaa",
+    "url": "/static/js/2.5b018ae3.chunk.js"
+  },
+  {
+    "revision": "a3f9a922bad92e7e4407",
+    "url": "/static/js/3.76d54c8d.chunk.js"
+  },
+  {
+    "revision": "c4303c16f97e3efc8136",
+    "url": "/static/js/main.4ce75b0d.chunk.js"
+  },
+  {
+    "revision": "a27f1bd3c029554ea771",
+    "url": "/static/js/runtime~main.aeb1f579.js"
+  }
+]);
